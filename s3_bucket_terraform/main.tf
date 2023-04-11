dd32dd391ee7b62bc971c0ca3038a95d0509bbeb
Mohammmedfareed22 <107261279+Mohammmedfareed22@users.noreply.github.com>
2023-04-11 17:52:18 +0530

    Update main.tf

--- s3_bucket_terraform/main.tf
+++ s3_bucket_terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 
-  access_key = "AKIATKAF7X7RH4JVIJ7L"
-  secret_key = "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"
+  access_key = "AKIATKA24aF7X7RH4JVIJ7L"
+  secret_key = "bsxl1U8H124fID+YcddI19G7Mjhm4xi6mTdtiKpFokw"
   region     = "ap-south-1"
 
 }
